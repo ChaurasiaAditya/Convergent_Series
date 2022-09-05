b1bddae5c3a5e8a3db7c19c8844d236cd9e3bedc
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-05 22:58:45 +0530

    Created calculateSumOfSeries method in ConvergentSeries.java class

--- src/main/java/com/aditya/ConvergentSeries.java
+++ src/main/java/com/aditya/ConvergentSeries.java
@@ -6,5 +6,20 @@
 package com.aditya;
 
 public class ConvergentSeries {
+    /**
+     * This method finds the sum of the series of numbers.
+     *
+     * @param number the number to find the sum of the series.
+     * @return the sum of the series.
+     */
+    public double calculateSumOfSeries(double number) {
+        double sum = 0.0;
+        for (int i = 2; i <= number; i++) {
+            double a = (1/Math.pow(2,i));
+            sum += a;
+        }
+        if (number>0)return sum + 1.0 + 0.5;
+        else return 0.0;
+    }
 
 }
